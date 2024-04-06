9c16e6585442435771cdaab63d1c0eb3903b317e
Jonathan Oswald <70522829+StreetLampLeMoose@users.noreply.github.com>
2024-04-05 22:04:42 -0600

    Update motor_run_stop.py
    removed adc read

--- motor_run_stop.py
+++ motor_run_stop.py
@@ -1,20 +1,12 @@
 import machine
-import utime
-import time
 motor1A = machine.Pin(14, machine.Pin.OUT)
 motor2A = machine.Pin(15, machine.Pin.OUT)
-adc = machine.ADC(machine.Pin(28))
 def motor_run_stop(run_state) :
-    while run_state == True:
-            time.sleep(0.25)
+    if run_state == True:
             motor1A.high()
             motor2A.low()
-            if adc.read_u16() < 1000 :
-                return 0
-                
-    while run_state == False:
-            time.sleep(0.25)
+            return print("motor running")     
+    if run_state == False:
             motor1A.low()
             motor2A.low()
-            if adc.read_u16() > 1000 :
-                return 0
+            return print("motor stopped")
