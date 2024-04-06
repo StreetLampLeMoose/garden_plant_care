642e643dbaea5acd1d2f0af8c5dd8d787e6e1c77
Jonathan Oswald <70522829+StreetLampLeMoose@users.noreply.github.com>
2024-04-05 22:06:31 -0600

    Update main.py
    extensive update. all functionality has been added and is complete. The light that is turned on is the internal pin 25 light but that can be changed easily.

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
-import motor_run_stop
-adc = machine.ADC(machine.Pin(28))
+pumpOnTime = 1
+pumpOnPeriod = 5
+lightOnTime = 1
+lightOnPeriod = 10
 
-while True:
+import motor_control
+import machine
+import time
+import _thread
+pin25 = machine.Pin(25, machine.Pin.OUT)
+pin16 = machine.Pin(16,machine.Pin.OUT)
+adc = machine.ADC(machine.Pin(28))
+lowLevel = 1000
+level = adc.read_u16()
+def lightOn() :
+    pin25.high()
+    return
+
+def lightOff() :
+    pin25.low()
+    return
+
+def pumpOnOff(timer,pumpOnTime) :
+    if level < lowLevel :
+        print("water level too low to run pump")
+        return
+    motor_control.motor_run_stop(True)
+    time.sleep(pumpOnTime)
+    motor_control.motor_run_stop(False) 
+    return
+
+timer =  machine.Timer()
+
+pumpStartTime = time.ticks_ms()
+lightStartTime = time.ticks_ms()
+lightOn()
+
+timer.init(mode = timer.PERIODIC, period = pumpOnPeriod*1000, callback = lambda t: pumpOnOff(t,pumpOnTime))
+ 
+while True :
     level = adc.read_u16()
-    while level > 1000:
+    if level < lowLevel :
+        pin16.high()
+    if level > lowLevel:
+        pin16.low()
+    while time.ticks_ms() - lightStartTime > lightOnTime *1000:
+        lightOff()
+        if time.ticks_ms() - lightStartTime > lightOnPeriod *1000 :  
+            lightStartTime = time.ticks_ms()
+            lightOn()
+            break
         
-        motor_run_stop.motor_run_stop(True)
-    while level < 1000 :
+            
+            
         
-        motor_run_stop.motor_run_stop(False)
         
+
